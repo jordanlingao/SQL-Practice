ef55dbddf8d63a49523b19af02c6a04154bf114d
jordanlingao <jordanlingao@gmail.com>
2022-12-30 21:54:28 -0500

    adds functions to shorten code

--- module_1/sqlite_example.py
+++ module_1/sqlite_example.py
@@ -1,19 +1,19 @@
-# Step 0 - import sqlite3
 import sqlite3
 import queries as q
 
-# Step 1 - connect to the database
-# triple-check spelling of database
-connection = sqlite3.connect("rpg_db.sqlite3")
+# DB Connect function
+def connect_to_db(db_name="rpg_db.sqlite3"):
+    return sqlite3.connect(db_name)
 
-# Step 2 - make cursor
-cursor = connection.cursor()
-
-# Step 3 - Write a query
-# See queries.py
-
-# Step 4 - Execute the query on the cursor and fetch the results
-results = cursor.execute(q.SELECT_ALL).fetchall()
+# Execute Query function
+def execute_q(conn, query):
+    # Make the cursor
+    curs = conn.cursor()
+    #Execute the query
+    curs.execute(query)
+    # Pull and return the results
+    return curs.fetchall()
 
 if __name__ == "__main__":
-    print(results[:5])
+    conn = connect_to_db()
+    print(execute_q(conn, q.SELECT_ALL)[:5])
